2ad272613ed82481853e75a03d00b68a055579d8
MimiMagusin <mimi@codaisseur.com>
2019-06-19 16:17:52 +0200

    Make it work

--- tests/checkForMultipleCommits.js
+++ tests/checkForMultipleCommits.js
@@ -3,12 +3,10 @@ const cp = require('child_process')
 let numberOfCommits = cp.execSync('git rev-list --count master', { encoding: 'utf8' })
 numberOfCommits = parseInt(numberOfCommits)
 
-if(numberOfCommits > 100){
+if(numberOfCommits > 1){
   console.log(`Well done! You have ${numberOfCommits} commits!`)
   process.exit(0)
 }
 
-if(numberOfCommits < 2){
-  console.log(`You have ${numberOfCommits} commits. That is fewer than the minimum 2 commits!`)
-  process.exit(1)
-}
+console.log(`You have ${numberOfCommits} commits. That is fewer than the minimum 2 commits!`)
+process.exit(1)
