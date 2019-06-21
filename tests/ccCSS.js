cf8e66772c1fa3369b84efdf3a5a371c1c2b6ffc
MimiMagusin <mimi@codaisseur.com>
2019-06-21 12:22:32 +0200

    is it the config object or isntalling?

--- tests/ccCSS.js
+++ tests/ccCSS.js
@@ -7,22 +7,7 @@ const formatSourceName = (path) => {
 
 stylelint.lint({
   config: {
-    rules: {
-      "at-rule-no-unknown": true,
-      "at-rule-semicolon-newline-after": "always",
-      "block-no-empty": true,
-      "block-opening-brace-newline-after": "always",
-      "color-no-invalid-hex": true,
-      "comment-no-empty": true,
-      "declaration-block-no-duplicate-properties": true,
-      "declaration-block-no-shorthand-property-overrides": true,
-      "font-family-no-duplicate-names": true,
-      "font-family-no-missing-generic-family-keyword": true,
-      "no-duplicate-selectors": true,
-      "no-empty-source": true,
-      "no-extra-semicolons": true,
-      "color-no-invalid-hex": true,
-    }
+    extends: 'stylelint-config-standard'
   },
   files: ".",
 })
